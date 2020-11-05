9a8a7aa947f43b19680a06ba25517ca07672b027
Shivam Naik <shivamnaik8239@gmail.com>
2020-11-05 16:26:33 +0530

    Added country and twitter api routes @kedar-devs

--- src/app.js
+++ src/app.js
@@ -3,10 +3,16 @@ const express = require('express')
 // Import the Weather Routes
 const weatherRouter = require('./weather/weatherRoutes')
 
+const Country = require('./model/Country')
+const Twitter = require('./model/Twitter')
+
 const app = express()
 
 // Use the Weather Routes
 app.use(weatherRouter)
+
+app.use(Country)
+app.use(Twitter)
 
 // export the app to index file
 module.exports = app
